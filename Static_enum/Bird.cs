bccca665a5d861b1ac63dbf9c399a073242b4173
User <User@WIN-8BH8LKBRQNO>
2023-11-04 15:21:07 +0500

    I added classes called Bird and BirdInformation

---
+++ Static_enum/Bird.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_enum
+{
+    public static class Bird
+    {
+        public static bool IsCanFly()
+        {
+            BirdInformation birdInformation = new BirdInformation();
+            if (birdInformation.canFly == true)
+            {
+                return true;
+
+            }
+            return false;
+        }
+
+        public static void SetBirdInformation()
+        {
+            BirdInformation birdInformation = new BirdInformation();
+            Console.Write("id: ");
+            birdInformation.id = Convert.ToInt32(Console.ReadLine());
+            Console.Write("name: ");
+            birdInformation.name = Console.ReadLine();
+            Console.Write("descreption: ");
+            birdInformation.description = Console.ReadLine();
+            Console.Write("type: ");
+            birdInformation.type = Console.ReadLine();
+
+        }
+    }
+}
